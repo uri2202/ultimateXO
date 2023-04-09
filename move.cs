b63543ba137766f93e3efbf07afbd8be21fc034c
uri2202 <uri1.barzilai@gmail.com>
2023-04-09 13:41:58 +0300

    aa

--- move.cs
+++ move.cs
@@ -10,5 +10,36 @@ class Move
     private int board;
     private int row;
     private int col;
+
+    public Move()
+    {
+       
+    }
+
+    public Move(int board, int row, int col)
+    {
+        this.board = board;
+        this.row = row;
+        this.col = col;
+    }
+    public int Board
+    {
+        get { return board; }
+        set { board = value; }
+    }
+
+    public int Row
+    {
+        get { return row; }
+        set { row = value; }
+    }
+
+    public int Col
+    {
+        get { return col; }
+        set { col = value; }
+    }
+
+
 }
 
